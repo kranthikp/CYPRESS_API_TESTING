b6b463e5ac046bc2653992a97f87cbeb529e9bf4
kranthikp7 <90747169+kranthikp7@users.noreply.github.com>
2023-05-28 15:27:39 +0530

    assertion to validate headers and status codes

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseURL: 'https://reqres.in//api/',
+    baseUrl: 'https://reqres.in/api/',
     screenshotOnRunFailure: false,
     video: false,
   },
--- cypress/e2e/3-api/api.cy.js
+++ cypress/e2e/3-api/api.cy.js
@@ -1,5 +1,40 @@
-describe('template spec', ()=>{
-    it('passed',()=>{
+/// <references types="cypres" />
+
+describe('API Testing with Cypress', ()=>{
+    it('passes',()=>{
         // TODO
+        cy.request('/users/2').then((response) =>{
+            cy.log(JSON.stringify(response.body))
+            // cy.log(JSON.stringify(response.body.data))
+            // cy.log(JSON.stringify(response.body.data.id))
+            // cy.log(JSON.stringify(response.body.data.email))
+            // cy.log(JSON.stringify(response.body.data.first_name))
+            cy.log(JSON.stringify(response.headers))
+        })
     })
+
+    it('API Test - Validate headers', () =>{
+        cy.request('/users/2').as('user')
+        cy.get('@user')
+            .its('headers')
+            .its('content-type')
+            .should('include', 'application/json')
+
+        cy.get('@user')
+            .its('headers')
+            .its('connection')
+            .should('include', 'keep-alive')
+    })
+
+    it('API Test - Status Codes', () => {
+        cy.request('/users/2').as('existingUser')
+        cy.get('@existingUser')
+            .its('status')
+            .should('equal', 200)
+
+        cy.request({url: '/users/non-exist', failOnStatusCode: false}).as('nonExistingUser')
+        cy.get('@nonExistingUser')
+        .its('status')
+        .should('equal', 404)
+    });
 })
